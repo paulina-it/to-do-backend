e4e2a6635c3eb78c0dc1b0368eea99c1a948fd70
Polina Bovykina <paulina.northy@gmail.com>
2024-10-21 20:08:19 +0100

    cors fixed

--- src/main/java/uk/bovykina/to_do/config/SecurityConfig.java
+++ src/main/java/uk/bovykina/to_do/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package uk.bovykina.to_do.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -21,8 +23,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow all OPTIONS requests
+                        .requestMatchers("/auth/signup", "/auth/login").permitAll() // Allow access to signup and login
+                        .anyRequest().authenticated() // Require authentication for all other requests
+                )
                 .httpBasic(withDefaults());
 
         return http.build();
